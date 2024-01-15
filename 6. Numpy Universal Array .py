cdfcac2f5d4d5337fc24b62e4212a4f881590d54
masudrana25 <mdmasudrana2512000@gmail.com>
2024-01-15 22:55:13 +0600

    Numpy Universal Array Function practice

---
+++ 6. Numpy Universal Array .py
@@ -0,0 +1,49 @@
+import numpy as np
+
+np1 = np.array([-3,-2,-1,0,1,2,3,4,5,6,7,8]) 
+
+#### square of each element
+# array_square = np.square(np1) ; print(array_square) 
+
+#### square-root of each element
+# array_square_root = np.sqrt(np1) ; print(array_square_root) 
+
+#### cube-root of each element = root-third 
+array_cube_root = np.cbrt(np1) ; print(array_cube_root) 
+
+#### power of each element
+# array_power = np.power(np1,3) ; print(array_power) 
+
+#### Negative of each element
+# array_negative = np.negative(np1) ; print(array_negative) 
+
+#### positive of each element
+# array_positive = np.positive(np1) ; print(array_positive) 
+
+#### absolute value of each element
+# array_absolute_value = np.abs(np1) ; print(array_absolute_value) 
+
+#### minimum of all element
+# array_min_element = np.min(np1) ; print(array_min_element) 
+
+#### maximum of all element
+# array_max_element = np.max(np1) ; print(array_max_element) 
+
+#### sign of all element
+# array_element_sign = np.sign(np1) ; print(array_element_sign) 
+
+#### exponent of each element
+# array_exponent = np.exp(np1) ; print(array_exponent)
+
+#### sin, cos, log of each element
+# array_log = np.log(np1) ; print(array_log)
+# array_sin = np.sin(np1) ; print(array_sin)
+
+
+a = np.array([-1.7, -1.5, -0.2, 0.2, 1.5, 1.7, 2.0])
+
+#### floor value of each element
+# array_element_floor_value = np.floor(a) ; print(array_element_floor_value)
+
+#### ceil value of each element
+# array_element_ceil_value = np.ceil(a) ; print(array_element_ceil_value)
