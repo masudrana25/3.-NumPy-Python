e74f1031836a868c51530a5298fb5d229f7c6860
masudrana25 <mdmasudrana2512000@gmail.com>
2024-01-18 02:17:47 +0600

    data type of numpy array

---
+++ 0.2. data type of numpy array .py
@@ -0,0 +1,30 @@
+import numpy as np 
+
+#### data type for boolean
+boolean_array = np.array([True, False, False,True]) ; print(boolean_array.dtype)
+
+#### data type for number 
+number_array_1 = np.array([1,2,3,4,5]) ; print(number_array_1.dtype)
+
+number_array_2 = np.array([1.1,2.2,3,4,5]) ; print(number_array_2.dtype)
+
+number_array_3 = np.array([1.11,2.22,3.32,4.53,5.54]) ; print(number_array_3.dtype)
+
+number_array_4 = np.array([1.13465453,2.2757872,3.578532,4.53347,5.55664]) ; print(number_array_4.dtype)
+
+#### data type for string
+
+string_array_1 = np.array(['Masud', 'sabbir','ibrahim']) ; print(string_array_1.dtype)
+
+string_array_2 = np.array(['Masud is ', 'sabbir is ','ibrahim is']) ; print(string_array_2.dtype)
+
+string_array_3 = np.array(['Masud is a ', 'sabbir is not a ','ibrahim is also a ']) ; print(string_array_3.dtype)
+
+string_array_4 = np.array(['Masud is a good', 'sabbir is not a bad','ibrahim is also a good']) ; print(string_array_4.dtype)
+
+string_array_5 = np.array(['Masud is a good boy', 'sabbir is not a bad boy','ibrahim is also a good boy']) ; print(string_array_5.dtype)
+
+string_array_6 = np.array(['Masud is a good boy who is so lazy', 'sabbir is not a bad boy who is not so lazy','ibrahim is also a good boy who is not so lazy']) ; print(string_array_6.dtype)
+
+string_array_7 = np.array(['Masud is a good boy who is so lazy sabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazysabbir is not a bad boy who is not so lazy' , 'sabbir is not a bad boy who is not so lazy' , 'ibrahim is also a good boy who is not so lazy']) ; print(string_array_7.dtype)
+
